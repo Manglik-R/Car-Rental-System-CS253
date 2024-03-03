23a55e1cee441a30dec3efb0991ece5ee9fec707
Raghav Manglik <134364536+Manglik-R@users.noreply.github.com>
2024-03-03 15:07:05 +0530

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -254,6 +254,10 @@ void Employee::clearDues() {
     cout << "-----------------------------------" << endl ;
     cout << "[NOTE] Your Current Dues to be paid: Rs. " << this -> getDues() << endl ;
     cout << "-----------------------------------" << endl ;
+    if( this -> getDues() == 0 ) {
+        cout << "No Dues to be paid!" << endl ;
+        return ;
+    }
     cout << "[Input] How much would you like to pay? (Rs.): " ;
     double pay ;
     string temp ;
